634b781ef0855339635728aab357c22370883cda
LeoP0 <edgar.psilvestre@gmail.com>
2019-11-11 20:11:11 -0800

    Updates

---
+++ precache-manifest.d6872f95d51a12d2fd1e22b02097de23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda84650ba51936a894806af338a63fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ade9565b9ed626378c8",
+    "url": "/static/js/2.816578f4.chunk.js"
+  },
+  {
+    "revision": "1a2a6c48d51e9ef37a71",
+    "url": "/static/js/main.5e1a60e7.chunk.js"
+  },
+  {
+    "revision": "de01a4b560b2ff56a902",
+    "url": "/static/js/runtime-main.842a5d88.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "2907dad7d72d9bb35e1e58d394cac032",
+    "url": "/static/media/leo.2907dad7.jpg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/static/media/linkedin.619231ff.svg"
+  }
+]);
